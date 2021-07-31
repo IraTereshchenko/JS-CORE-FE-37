b2160f664f4782a43f3b002dbfd8b5e3d467a17f
IraTereshchenko <tereshchenkoim@gmail.com>
2021-07-31 04:47:00 +0300

    js module 2-1
    js module 2-1

---
+++ js/module-2-1.js
@@ -0,0 +1,65 @@
+// ARRAY
+//const array = [1, 2, 3, "Java Script", 4, 5, 6]
+//console.log(array)
+
+// array length -кількість елементів в масиві
+//console.log(array.length)
+
+// read (get) element by arrays
+//console.log(array[0])
+//console.log(array[3])
+
+// update (patch) element by array
+//array[0] = 'I Love'
+//console.log(array[0])
+//console.log(array)
+
+//delete element in array
+//delete array[1]
+//delete array[2]
+//console.log(array[1])
+//console.log(array[2])
+//console.log(array)
+
+//add elem by index
+//array[1] = 'HTML'
+//array[2] = 'CSS'
+//console.log(array)
+
+//iterate by array (for & for ..of)
+//break & continue
+//                               i = i + 1
+//                               i++                                   
+//for (let i = 0; i < array.length; i += 1) {
+    //console.log('Break')
+    //if (typeof array[i] === 'string') {
+       //break
+    //}
+      // console.log(`iтерація - ${i + 1}`, array[i])
+        
+    //console.log('index:', i)
+    // ітерація 1 - array[0] - I Love
+    //ітерація 2 - array[1] - 2
+    //ітерація 3 - array[2] - 3
+    //ітерація 4 - array[3] - Java Script
+    //ітерація 5 - array[4] - 4
+    //ітерація 6 - array[5] - 5
+    //ітерація 7 - array[6] - 6
+//}
+
+//multidimensional arrays
+
+//const multiArray = [
+    //[1, 1, 1],
+    //[2, 2, 2],
+   // [3, 3, 3],
+//]
+//console.log(multiArray.length)
+//console.log(multiArray)
+//console.log(multiArray[0])
+
+//for (let array of multiArray) {
+   // console.log(array)
+//}
+
+
