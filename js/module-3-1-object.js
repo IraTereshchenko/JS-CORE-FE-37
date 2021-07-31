e60a900ca68d08d3d7713c549e6d8f2c51884710
IraTereshchenko <tereshchenkoim@gmail.com>
2021-07-31 04:47:29 +0300

    js module 3-1
    js module 3-1

---
+++ js/module-3-1-object.js
@@ -0,0 +1,55 @@
+/*// console.log(typeof {}, {})
+console.log({})
+
+const obj = {
+    //props (key: value,)
+    props_a: 'A',
+    props_b; 'B',
+    props_c: 2,
+    props_d: null,
+    props_e: true,
+    name: 'Iryna',
+    age: 38,
+    isOnline: false,
+    inStudent: true,
+    skills: ['HTML', 'CSS'],
+    music: {
+        songs: ['ggggg']
+    },
+}
+//methods
+    //ES6
+    //showName() {
+       // console.log('Hello')
+   // },
+
+console.log(obj)
+
+
+//Зміна назви  свойства і обєкта
+console.log(obj.age)
+obj.age = 18
+console.log(obj.age);
+
+//SHORT  props
+let age = 30
+console.log(age);
+const skills = ['HTML', CSS]
+
+const myUser = {
+    age,
+    skills,
+}
+console.log(myUser);
+
+
+
+// вичисляемие свойства
+
+let key1 = 'name'
+let key2 = 'lastName'
+let key3 = 'skills'
+
+const nexUser = {
+    [key1]: 'user',
+}*/
