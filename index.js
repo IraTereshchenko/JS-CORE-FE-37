53809231504e2aea58ca1d41578600738a257d9c
IraTereshchenko <tereshchenkoim@gmail.com>
2021-07-31 04:45:40 +0300

    Підключення index.js
    Підключення index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-import './js/module-1-1';
+import './js/module-1-1.js';
+import './js/module-1-2.js';
+import './js/module-2-1.js';
+import './js/module-2-2.js';
+import './js/module-3-1-object.js';
+import '/js/hw-3.js';
